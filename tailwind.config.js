abab1e88855f9fbfec8d42f231e35b02f0b1a2bf
Adhavan <ap4992@srmist.edu.in>
2023-03-13 14:43:37 +0530

    Webdev
    ..

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./templates/**/*.html",
+            "./static/css/src/**/*.js"
+            ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
